8730fa0c87f239cce1f30c6479137223e062d1c4
NATHAN VETTE RUER <nathan.vetteruer21@clcmbp23.local>
2019-04-30 08:51:23 -0500

    Initial Commit

---
+++ MLB Info App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MLB Info App
+//
+//  Created by NATHAN VETTE RUER on 4/26/19.
+//  Copyright © 2019 clc.vetteruer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
