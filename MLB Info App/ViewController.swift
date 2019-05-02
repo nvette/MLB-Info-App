cb89c7ace017890bb215b8762fe3d71f4a43b909
ESTEBAN REYES PEREZ <esteban.reyesperez21@clcmbp17.local>
2019-05-02 08:51:55 -0500

    Import safari

--- MLB Info App/ViewController.swift
+++ MLB Info App/ViewController.swift
@@ -7,12 +7,11 @@
 //
 // heyy
 import UIKit
-
+import SafariServices
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
