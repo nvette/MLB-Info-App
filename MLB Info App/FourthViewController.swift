99a4f2c752de759759717f1d8f8bd9d61dddcd2f
ESTEBAN REYES PEREZ <esteban.reyesperez21@clcmbp17.local>
2019-05-13 09:15:28 -0500

    ok

---
+++ MLB Info App/FourthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FourthViewController.swift
+//  MLB Info App
+//
+//  Created by ESTEBAN REYES PEREZ on 5/13/19.
+//  Copyright © 2019 clc.vetteruer. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
