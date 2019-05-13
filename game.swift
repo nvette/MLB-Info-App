443fdc724e43f703045b7305c6916b8ec757cebe
NATHAN VETTE RUER <nathan.vetteruer21@clcmbp23.local>
2019-05-13 09:11:20 -0500

    fixed some things up

---
+++ game.swift
@@ -0,0 +1,30 @@
+//
+//  game.swift
+//  MLB Info App
+//
+//  Created by NATHAN VETTE RUER on 5/13/19.
+//  Copyright © 2019 clc.vetteruer. All rights reserved.
+//
+
+import UIKit
+
+class game: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
